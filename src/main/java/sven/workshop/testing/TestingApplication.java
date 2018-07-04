811ff7a92482a311d97e788cd20b3b59a4e42476
Sven Wesley <sven.wesley@cygni.se>
2018-07-04 13:14:05 +0200

    Groovy setup

--- src/main/java/sven/workshop/testing/TestingApplication.java
+++ src/main/java/sven/workshop/testing/TestingApplication.java
@@ -1,12 +1,15 @@
 package sven.workshop.testing;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class TestingApplication {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(TestingApplication.class, args);
 	}
 }
